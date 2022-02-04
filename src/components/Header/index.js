6d84fcaf6f186a008f50c6bdc2fb3f5278cde4a9
kumaribethapudi19 <kumaribethapudi19@gmail.com>
2022-02-04 11:59:17 +0000

    My bookhub header included

---
+++ src/components/Header/index.js
@@ -0,0 +1,82 @@
+import {Link, withRouter} from 'react-router-dom'
+
+import Cookies from 'js-cookie'
+
+import {GiHamburgerMenu} from 'react-icons/gi'
+import {AiFillCloseCircle} from 'react-icons/ai'
+
+import './index.css'
+
+const Header = props => {
+  const onClickLogout = () => {
+    Cookies.remove('jwt_token')
+    const {history} = props
+    history.replace('/login')
+  }
+
+  const renderHamburgerMenu = () => {
+    console.log('hambrgr  menu clicked ')
+    return (
+      <>
+        <ul className="nav-menu-list-mobile">
+          <Link to="/">
+            <li className="nav-menu-item-mobile">Home</li>
+          </Link>
+          <Link to="/shelves">
+            <li className="nav-menu-item-mobile">BookShelves</li>
+          </Link>
+          <button
+            type="button"
+            className="logout-mobile-btn"
+            onClick={onClickLogout}
+          >
+            Logout
+          </button>
+          <AiFillCloseCircle />
+        </ul>
+      </>
+    )
+  }
+
+  return (
+    <nav className="nav-header">
+      <div className="nav-content">
+        <Link to="/">
+          <img
+            className="website-logo"
+            src="https://res.cloudinary.com/dp7ibjh2t/image/upload/v1643911378/BookHub/Bkhub_title_b9mwj8.png"
+            alt="website logo"
+          />
+        </Link>
+        <ul className="nav-menu">
+          <Link to="/" className="nav-link">
+            <li>Home</li>
+          </Link>
+          <Link to="/shelves" className="nav-link">
+            <li>BookShelves</li>
+          </Link>
+          <button
+            type="button"
+            className="logout-desktop-btn"
+            onClick={onClickLogout}
+          >
+            Logout
+          </button>
+        </ul>
+      </div>
+      <div className="nav-menu-mobile">
+        <Link to="/">
+          <img
+            className="website-logo"
+            src="https://res.cloudinary.com/dp7ibjh2t/image/upload/v1643911378/BookHub/Bkhub_title_b9mwj8.png"
+            alt="website logo"
+          />
+        </Link>
+        <button onClick={renderHamburgerMenu}>
+          <GiHamburgerMenu />
+        </button>
+      </div>
+    </nav>
+  )
+}
+export default withRouter(Header)
--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,3 +1,22 @@
-const Home = () => <h1>Home page</h1>
+import Header from '../Header'
 
+const Home = () => (
+  <div className="home-page-container">
+    <Header />
+    <h1 className="home-page-heading">Find Your Next Favorite Books?</h1>
+    <p className="home-page-description">
+      You are in the right place. Tell us what titles or genres you have enjoyed
+      in the past, and we will give you surprisingly insightful recommendations.
+    </p>
+    <button className="home-page-find-button">Find Books</button>
+    <div className="books-display-card">
+      <h1 className="display-heading">Top Rated Books</h1>
+      <button className="home-page-find-button">Find Books</button>
+      <h1>Carousal Should Come here</h1>
+    </div>
+    <div className="footer-section">
+      <h1>Icons Should come here</h1>
+    </div>
+  </div>
+)
 export default Home
