2601f4405cd00223b3158c6f80a4a912a8143e16
kumaribethapudi19 <kumaribethapudi19@gmail.com>
2022-02-04 18:19:56 +0000

    my bookhub header updated1

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,83 +1,96 @@
 import {Link, withRouter} from 'react-router-dom'
+import {Component} from 'react'
 import Cookies from 'js-cookie'
 import {GiHamburgerMenu} from 'react-icons/gi'
 import {AiFillCloseCircle} from 'react-icons/ai'
 import './index.css'
 
-const Header = props => {
-  const onClickLogout = () => {
+class Header extends Component {
+  state = {isMenuClicked: false}
+
+  onClickLogout = () => {
     Cookies.remove('jwt_token')
-    const {history} = props
+    const {history} = this.props
     history.replace('/login')
   }
 
-  const renderHamburgerMenu = () => {
+  renderHamburgerMenu = () => {
     console.log('hambrgr  menu clicked ')
-    return (
-      <div id="hamburger-menu-expanded">
-        <ul className="nav-menu-list-mobile">
-          <Link to="/">
-            <li className="nav-menu-item-mobile">Home</li>
-          </Link>
-          <Link to="/shelves">
-            <li className="nav-menu-item-mobile">BookShelves</li>
-          </Link>
-          <button
-            type="button"
-            className="logout-mobile-btn"
-            onClick={onClickLogout}
-          >
-            Logout
-          </button>
-          <AiFillCloseCircle />
-        </ul>
-      </div>
-    )
+    this.setState(prevState => ({isButtonClicked: !prevState.isButtonClicked}))
   }
 
-  return (
-    <>
-      <nav className="nav-header">
-        <div className="nav-content">
-          <Link to="/">
-            <img
-              className="website-logo"
-              src="https://res.cloudinary.com/dp7ibjh2t/image/upload/v1643911378/BookHub/Bkhub_title_b9mwj8.png"
-              alt="website logo"
-            />
-          </Link>
-          <ul className="nav-menu">
-            <Link to="/" className="nav-link">
-              <li>Home</li>
-            </Link>
-            <Link to="/shelves" className="nav-link">
-              <li>BookShelves</li>
-            </Link>
-            <button
-              type="button"
-              className="logout-desktop-btn"
-              onClick={onClickLogout}
-            >
-              Logout
-            </button>
-          </ul>
-        </div>
-        <div className="nav-menu-mobile">
-          <Link to="/">
-            <img
-              className="website-logo"
-              src="https://res.cloudinary.com/dp7ibjh2t/image/upload/v1643911378/BookHub/Bkhub_title_b9mwj8.png"
-              alt="website logo"
-            />
-          </Link>
-
-          <button onClick={renderHamburgerMenu} className="hamburger-icon">
-            <GiHamburgerMenu />
-          </button>
-        </div>
-      </nav>
-      <div className="ham-menu-display">{renderHamburgerMenu()}</div>
-    </>
+  renderMenu = () => (
+    <div id="hamburger-menu-expanded">
+      <ul className="nav-menu-list-mobile">
+        <Link to="/">
+          <li className="nav-menu-item-mobile">Home</li>
+        </Link>
+        <Link to="/shelves">
+          <li className="nav-menu-item-mobile">BookShelves</li>
+        </Link>
+        <button
+          type="button"
+          className="logout-mobile-btn"
+          onClick={this.onClickLogout}
+        >
+          Logout
+        </button>
+        <AiFillCloseCircle />
+      </ul>
+    </div>
   )
+
+  render() {
+    const {isButtonClicked} = this.state
+    return (
+      <>
+        <nav className="nav-header">
+          <div className="nav-content">
+            <Link to="/">
+              <img
+                className="website-logo"
+                src="https://res.cloudinary.com/dp7ibjh2t/image/upload/v1643911378/BookHub/Bkhub_title_b9mwj8.png"
+                alt="website logo"
+              />
+            </Link>
+            <ul className="nav-menu">
+              <Link to="/" className="nav-link">
+                <li>Home</li>
+              </Link>
+              <Link to="/shelves" className="nav-link">
+                <li>BookShelves</li>
+              </Link>
+              <button
+                type="button"
+                className="logout-desktop-btn"
+                onClick={this.onClickLogout}
+              >
+                Logout
+              </button>
+            </ul>
+          </div>
+          <div className="nav-menu-mobile">
+            <Link to="/">
+              <img
+                className="website-logo"
+                src="https://res.cloudinary.com/dp7ibjh2t/image/upload/v1643911378/BookHub/Bkhub_title_b9mwj8.png"
+                alt="website logo"
+              />
+            </Link>
+
+            <button
+              onClick={this.renderHamburgerMenu}
+              className="hamburger-icon"
+            >
+              <GiHamburgerMenu />
+            </button>
+          </div>
+        </nav>
+        <div className="ham-menu-display">
+          isButtonClicked && {this.renderMenu()}
+        </div>
+      </>
+    )
+  }
 }
 export default withRouter(Header)
