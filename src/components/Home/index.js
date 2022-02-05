56d8ec570f0cad9c65030b8f162f989fa3f4c92d
kumaribethapudi19 <kumaribethapudi19@gmail.com>
2022-02-05 10:30:46 +0000

    my bookhub home updations2

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,32 +1,53 @@
+import {Component} from 'react'
+
 import Header from '../Header'
+
 import './index.css'
 
-const Home = () => (
-  <div className="home-page-container">
-    <Header />
-    <div className="home-description-card">
-      <h1 className="home-page-heading">Find Your Next Favorite Books?</h1>
-      <p className="home-page-description">
-        You are in the right place. Tell us what titles or genres you have
-        enjoyed in the past, and we will give you surprisingly insightful
-        recommendations.
-      </p>
-      <button className="home-page-find-button">Find Books</button>
-    </div>
-    <div className="books-display-card">
-      <div className="books-display-card-header">
-        <h1 className="display-heading">
-          Top Rated Books
-          <span>
-            <button className="display-card-find-button">Find Books</button>{' '}
-          </span>
-        </h1>
+class Home extends Component {
+  state = {
+    topRatedBooksList: [],
+  }
+
+  onClickFindBooks = () => {
+    console.log('Find Books Clicked')
+  }
+
+  render() {
+    const {topRatedBooksList} = this.state
+    return (
+      <div className="home-page-container">
+        <Header />
+        <div className="home-description-card">
+          <h1 className="home-page-heading">Find Your Next Favorite Books?</h1>
+          <p className="home-page-description">
+            You are in the right place. Tell us what titles or genres you have
+            enjoyed in the past, and we will give you surprisingly insightful
+            recommendations.
+          </p>
+          <button
+            className="home-page-find-button"
+            onClick={this.onClickFindBooks}
+          >
+            Find Books
+          </button>
+        </div>
+        <div className="books-display-card">
+          <div className="books-display-card-header">
+            <h1 className="display-heading">
+              Top Rated Books
+              <span>
+                <button className="display-card-find-button">Find Books</button>{' '}
+              </span>
+            </h1>
+          </div>
+          <h1>Carousal Should Come here</h1>
+        </div>
+        <div className="footer-section">
+          <h1>Icons Should come here</h1>
+        </div>
       </div>
-      <h1>Carousal Should Come here</h1>
-    </div>
-    <div className="footer-section">
-      <h1>Icons Should come here</h1>
-    </div>
-  </div>
-)
+    )
+  }
+}
 export default Home
