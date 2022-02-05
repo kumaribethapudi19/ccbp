e53b8695a7156938110b95a4e5e3a77c35d82761
kumaribethapudi19 <kumaribethapudi19@gmail.com>
2022-02-05 05:06:16 +0000

    my bookhub changes in home mobile view

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,14 +1,18 @@
 import Header from '../Header'
+import './index.css'
 
 const Home = () => (
   <div className="home-page-container">
     <Header />
-    <h1 className="home-page-heading">Find Your Next Favorite Books?</h1>
-    <p className="home-page-description">
-      You are in the right place. Tell us what titles or genres you have enjoyed
-      in the past, and we will give you surprisingly insightful recommendations.
-    </p>
-    <button className="home-page-find-button">Find Books</button>
+    <div className="home-description-card">
+      <h1 className="home-page-heading">Find Your Next Favorite Books?</h1>
+      <p className="home-page-description">
+        You are in the right place. Tell us what titles or genres you have
+        enjoyed in the past, and we will give you surprisingly insightful
+        recommendations.
+      </p>
+      <button className="home-page-find-button">Find Books</button>
+    </div>
     <div className="books-display-card">
       <h1 className="display-heading">Top Rated Books</h1>
       <button className="home-page-find-button">Find Books</button>
