76a9bd1148c57200d9570dbf612e784e1600c6e7
kumaribethapudi19 <kumaribethapudi19@gmail.com>
2022-02-06 15:42:24 +0000

    slick added to my bookhub

--- src/components/BooksSlick/index.js
+++ src/components/BooksSlick/index.js
@@ -10,24 +10,22 @@ const BooksSlick = props => {
   console.log(booksList)
   const settings = {
     dots: true,
-    slidesToShow: 3,
+    slidesToShow: 1,
     slidesToScroll: 1,
   }
   return (
     <div className="slider-container">
       <Slider {...settings}>
-        <ul>
-          {booksList.map(each => {
-            const {coverPic, title, authorName} = each
-            return (
-              <>
-                <img src={each.coverPic} />
-                <h1>{each.title}</h1>
-                <h3>{each.authorName} </h3>
-              </>
-            )
-          })}
-        </ul>
+        {booksList.map(each => {
+          const {coverPic, title, authorName, id} = each
+          return (
+            <div key={each.id}>
+              <img src={each.coverPic} />
+              <h1>{each.title}</h1>
+              <h3>{each.authorName} </h3>
+            </div>
+          )
+        })}
       </Slider>
     </div>
   )
