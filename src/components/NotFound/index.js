700911364e6d19af6217e3cc3348a3e35e60fcef
kumaribethapudi19 <kumaribethapudi19@gmail.com>
2022-02-28 17:19:16 +0000

    not found modified1

--- src/components/NotFound/index.js
+++ src/components/NotFound/index.js
@@ -19,14 +19,15 @@ const NotFound = props => {
         we are sorry, the page you requested could not be found. Please go back
         to the homepage.
       </p>
-
-      <button
-        type="button"
-        className="not-found-button"
-        onClick={goBackToHomeButtonClicked}
-      >
-        Go Back to Home
-      </button>
+      <Link to="/">
+        <button
+          type="button"
+          className="not-found-button"
+          onClick={goBackToHomeButtonClicked}
+        >
+          Go Back to Home
+        </button>
+      </Link>
     </div>
   )
 }
