5c35acab2a69cc4beafcc4de1b6eaa8815259cdf
kumaribethapudi19 <kumaribethapudi19@gmail.com>
2022-03-02 10:27:40 +0000

    my bookhub home modified

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -121,8 +121,64 @@ class Home extends Component {
     </div>
   )
 
-  renderTopRatedBooksListView = () => {
-    const {topRatedBooksList} = this.state
+  renderTopRatedBooksListView = () => this.renderTopRatedBooksList()
+
+  renderFailureView = () => (
+    <div className="books-display-card">
+      <div className="books-display-card-header">
+        <h1 className="display-card-heading">
+          Top Rated Books
+          <span>
+            <button
+              type="button"
+              className="display-card-find-button"
+              onClick={this.onClickFindBooks}
+            >
+              Find Books
+            </button>
+          </span>
+        </h1>
+      </div>
+      <div
+        className="something-wrong-view-container"
+        testid="somethingWrongViewContainer"
+      >
+        <img
+          alt="failure view"
+          className="something-wrong-view"
+          src="https://res.cloudinary.com/dp7ibjh2t/image/upload/v1644112090/BookHub/SmthngwntWrong_dbyzgy.png"
+        />
+        <p className="something-wrong-heading">
+          Something went wrong. Please try again
+        </p>
+        <button
+          type="button"
+          className="something-wrong-try-again-button"
+          onClick={this.onTryAgainButtonClicked}
+        >
+          Try Again
+        </button>
+      </div>
+    </div>
+  )
+
+  renderViews = () => {
+    const {apiStatus} = this.state
+
+    switch (apiStatus) {
+      case apiStatusConstants.success:
+        return this.renderTopRatedBooksListView()
+      case apiStatusConstants.failure:
+        return this.renderFailureView()
+      case apiStatusConstants.inProgress:
+        return this.renderLoadingView()
+      default:
+        return null
+    }
+  }
+
+  render() {
+    const {apiStatus} = this.state
     return (
       <div>
         <div className="header-container">
@@ -146,23 +202,7 @@ class Home extends Component {
               Find Books
             </button>
           </div>
-          <div className="books-display-card">
-            <div className="books-display-card-header">
-              <h1 className="display-card-heading">
-                Top Rated Books
-                <span>
-                  <button
-                    type="button"
-                    className="display-card-find-button"
-                    onClick={this.onClickFindBooks}
-                  >
-                    Find Books
-                  </button>
-                </span>
-              </h1>
-            </div>
-            {this.renderTopRatedBooksList()}
-          </div>
+          {this.renderViews()}
 
           <div className="footer-section">
             <div className="footer-icons-container">
@@ -176,92 +216,6 @@ class Home extends Component {
         </div>
       </div>
     )
-  }
-
-  renderFailureView = () => (
-    <div>
-      <div className="header-container">
-        <Header />
-      </div>
-      <div className="body-container">
-        <div className="home-description-card">
-          <h1 className="home-page-heading">Find Your Next Favorite Books?</h1>
-          <p className="home-page-description">
-            You are in the right place. Tell us what titles or genres you have
-            enjoyed in the past, and we will give you surprisingly insightful
-            recommendations.
-          </p>
-          <button
-            className="home-page-find-button"
-            type="button"
-            onClick={this.onClickFindBooks}
-          >
-            Find Books
-          </button>
-        </div>
-        <div className="books-display-card">
-          <div className="books-display-card-header">
-            <h1 className="display-card-heading">
-              Top Rated Books
-              <span>
-                <button
-                  type="button"
-                  className="display-card-find-button"
-                  onClick={this.onClickFindBooks}
-                >
-                  Find Books
-                </button>
-              </span>
-            </h1>
-          </div>
-          <div
-            className="something-wrong-view-container"
-            testid="somethingWrongViewContainer"
-          >
-            <img
-              alt="failure view"
-              className="something-wrong-view"
-              src="https://res.cloudinary.com/dp7ibjh2t/image/upload/v1644112090/BookHub/SmthngwntWrong_dbyzgy.png"
-            />
-            <p className="something-wrong-heading">
-              Something went wrong. Please try again
-            </p>
-            <button
-              type="button"
-              className="something-wrong-try-again-button"
-              onClick={this.onTryAgainButtonClicked}
-            >
-              Try Again
-            </button>
-          </div>
-        </div>
-
-        <div className="footer-section">
-          <div className="footer-icons-container">
-            <FaGoogle className="icon-style" />
-            <FaTwitter className="icon-style" />
-            <FaInstagram className="icon-style" />
-            <FaYoutube className="icon-style" />
-          </div>
-          <p className="footer-note">Contact us</p>
-        </div>
-      </div>
-    </div>
-  )
-
-  render() {
-    const {apiStatus} = this.state
-
-    switch (apiStatus) {
-      case apiStatusConstants.success:
-        return this.renderTopRatedBooksListView()
-      case apiStatusConstants.failure:
-        return this.renderFailureView()
-      case apiStatusConstants.inProgress:
-        return this.renderLoadingView()
-      default:
-        return null
-    }
   }
 }
 
