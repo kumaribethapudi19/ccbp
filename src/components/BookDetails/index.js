1c49f22c16f168dff77729aa154fa77b418d87b3
kumaribethapudi19 <kumaribethapudi19@gmail.com>
2022-03-05 04:49:59 +0000

    bookhub mini project bookdetails route modified

--- src/components/BookDetails/index.js
+++ src/components/BookDetails/index.js
@@ -86,71 +86,47 @@ class BookDetails extends Component {
       title,
     } = bookData
     return (
-      <div>
-        <div className="header-container">
-          <Header />
-        </div>
-        <div className="body-container">
-          <div className="book-details-container">
-            <div className="cover-details">
-              <img src={coverPic} alt={title} className="cover-pic-style" />
-              <div className="cover-details-card">
-                <h1 className="main-heading">{title}</h1>
-                <p className="cover-description">{authorName}</p>
-                <p className="cover-description">
-                  Avg Rating <BsFillStarFill className="star" />
-                  {rating}
-                </p>
-                <p className="cover-description status-style">
-                  Status: {readStatus}
-                </p>
-              </div>
-            </div>
-            <br className="line-style" />
-            <h1 className="heading">About Author</h1>
-            <p className="description">{aboutAuthor}</p>
-            <h1 className="heading">About Book</h1>
-            <p className="description">{aboutBook}</p>
-          </div>
-
-          <div className="footer-section">
-            <div className="footer-icons-container">
-              <FaGoogle className="icon-style" />
-              <FaTwitter className="icon-style" />
-              <FaInstagram className="icon-style" />
-              <FaYoutube className="icon-style" />
-            </div>
-            <p className="footer-note">Contact us</p>
+      <div className="book-details-container">
+        <div className="cover-details">
+          <img src={coverPic} alt={title} className="cover-pic-style" />
+          <div className="cover-details-card">
+            <h1 className="main-heading">{title}</h1>
+            <p className="cover-description">{authorName}</p>
+            <p className="cover-description">
+              Avg Rating <BsFillStarFill className="star" />
+              {rating}
+            </p>
+            <p className="cover-description status-style">
+              Status: {readStatus}
+            </p>
           </div>
         </div>
+        <br className="line-style" />
+        <h1 className="heading">About Author</h1>
+        <p className="description">{aboutAuthor}</p>
+        <h1 className="heading">About Book</h1>
+        <p className="description">{aboutBook}</p>
       </div>
     )
   }
 
   renderFailureView = () => (
-    <div className="home-page-container">
-      <div className="header-container">
-        <Header />
-      </div>
-      <div className="body-container">
-        <div className="something-wrong-view-container">
-          <img
-            alt="failure view"
-            className="something-wrong-view"
-            src="https://res.cloudinary.com/dp7ibjh2t/image/upload/v1644112090/BookHub/SmthngwntWrong_dbyzgy.png"
-          />
-          <p className="something-wrong-heading">
-            Something went wrong. Please try again
-          </p>
-          <button
-            type="button"
-            className="something-wrong-try-again-button"
-            onClick={this.onTryAgainButtonClicked}
-          >
-            Try Again
-          </button>
-        </div>
-      </div>
+    <div className="something-wrong-view-container">
+      <img
+        alt="failure view"
+        className="something-wrong-view"
+        src="https://res.cloudinary.com/dp7ibjh2t/image/upload/v1644112090/BookHub/SmthngwntWrong_dbyzgy.png"
+      />
+      <p className="something-wrong-heading">
+        Something went wrong. Please try again
+      </p>
+      <button
+        type="button"
+        className="something-wrong-try-again-button"
+        onClick={this.onTryAgainButtonClicked}
+      >
+        Try Again
+      </button>
     </div>
   )
 
@@ -160,7 +136,7 @@ class BookDetails extends Component {
     </div>
   )
 
-  render() {
+  renderViews = () => {
     const {apiStatus} = this.state
     switch (apiStatus) {
       case apiStatusConstants.success:
@@ -172,6 +148,27 @@ class BookDetails extends Component {
       default:
         return null
     }
+  }
+
+  render() {
+    const {apiStatus} = this.state
+    return (
+      <div>
+        <div className="header-container">
+          <Header />
+        </div>
+        <div className="body-container">{this.renderViews()}</div>
+        <div className="footer-section">
+          <div className="footer-icons-container">
+            <FaGoogle className="icon-style" />
+            <FaTwitter className="icon-style" />
+            <FaInstagram className="icon-style" />
+            <FaYoutube className="icon-style" />
+          </div>
+          <p className="footer-note">Contact us</p>
+        </div>
+      </div>
+    )
   }
 }
 export default BookDetails
