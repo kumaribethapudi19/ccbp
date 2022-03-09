020a7f569e2427db39f338db0b096fe8bdec59f2
kumaribethapudi19 <kumaribethapudi19@gmail.com>
2022-03-09 19:46:36 +0000

    bookhub home modified

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -103,7 +103,7 @@ class Home extends Component {
           {topRatedBooksList.map(eachBook => {
             const {coverPic, title, authorName, id} = eachBook
             return (
-              <li className="top-rated-book-card-style">
+              <li className="top-rated-book-card-style" key={id}>
                 <Link to={`/books/${id}`}>
                   <img
                     className="top-rated-book-pic-style"
